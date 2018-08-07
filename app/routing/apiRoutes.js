019899a87077fdb9797d14db98e489493d989319
Carol Martin <carolmartin@cox.net>
2018-08-06 19:27:16 -0700

    updated logic with search friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,6 +20,56 @@ module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
+
+  });
+// API POST request from Tucker- need to have something to listen to the post request and process request
+  app.post("/api/friends", function(req, res){
+    console.log(req.body);
+    var user = req.body;
+
+    var closestDiff = 0;
+    var closestFriendIndex;
+    
+    var totalDiff = [];
+  
+
+    // now loop through the friends to find the closest match
+    for (i= 0; i < friendsData.length; i++) {
+      totalDiff[i] = 0;
+
+      var diff = 0;
+      // loop through the scores of the friend and find differences
+      // between the friends and users scores. 
+      // 
+      for (var x = 0; x < 10; x++){
+        console.log("friends score index " + friendsData[i].scores[x]);
+        console.log("user score: " + user.scores[x]);
+      
+        diff = parseInt(friendsData[i].scores[x]) - parseInt(user.scores[x]);
+        console.log ("diff is : " + diff);
+        // make sure difference is a positive number
+        if (diff < 0) {
+          diff = diff * -1;
+        }
+        // sum of the total difference for this particular friend
+        totalDiff[i] += diff;
+      }
+    }
+    //find the index of the lowest toltalDiff and thsi will be the most compatable friend
+    var lowestDiff = 50;
+    for (i=0; i < totalDiff.length; i++) {
+      if (totalDiff[i] < lowestDiff) {
+        lowestDiff = parseInt(totalDiff[i]);
+      }
+    }
+
+    console.log("closest friend match is index " + i + " with match score difference of " + lowestDiff);
+
+
+    friendsData.push(req.body);
+    console.log("in apiRoutes post");
+    
+
   });
 
     // API POST Requests
